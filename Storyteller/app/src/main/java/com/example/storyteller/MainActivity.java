a4bd20552f40d30df31959abb619f9401a6cd4a1
Courtnie Chan <courtnie@berkeley.edu>
2019-11-22 14:59:28 -0800

    completed book tutorial and added floating action button to main activity

--- Storyteller/app/src/main/java/com/example/storyteller/MainActivity.java
+++ Storyteller/app/src/main/java/com/example/storyteller/MainActivity.java
@@ -1,14 +1,58 @@
 package com.example.storyteller;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    List<Book> lstBook ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lstBook = new ArrayList<>();
+        lstBook.add(new Book("The Vegitarian","Categorie Book","Description book",R.drawable.thevigitarian));
+        lstBook.add(new Book("The Wild Robot","Categorie Book","Description book",R.drawable.thewildrobot));
+        lstBook.add(new Book("Maria Semples","Categorie Book","Description book",R.drawable.mariasemples));
+        lstBook.add(new Book("The Martian","Categorie Book","Description book",R.drawable.themartian));
+        lstBook.add(new Book("He Died with...","Categorie Book","Description book",R.drawable.hediedwith));
+        lstBook.add(new Book("The Vegitarian","Categorie Book","Description book",R.drawable.thevigitarian));
+        lstBook.add(new Book("The Wild Robot","Categorie Book","Description book",R.drawable.thewildrobot));
+        lstBook.add(new Book("Maria Semples","Categorie Book","Description book",R.drawable.mariasemples));
+        lstBook.add(new Book("The Martian","Categorie Book","Description book",R.drawable.themartian));
+        lstBook.add(new Book("He Died with...","Categorie Book","Description book",R.drawable.hediedwith));
+        lstBook.add(new Book("The Vegitarian","Categorie Book","Description book",R.drawable.thevigitarian));
+        lstBook.add(new Book("The Wild Robot","Categorie Book","Description book",R.drawable.thewildrobot));
+        lstBook.add(new Book("Maria Semples","Categorie Book","Description book",R.drawable.mariasemples));
+        lstBook.add(new Book("The Martian","Categorie Book","Description book",R.drawable.themartian));
+        lstBook.add(new Book("He Died with...","Categorie Book","Description book",R.drawable.hediedwith));
+
+        RecyclerView myrv = (RecyclerView) findViewById(R.id.recyclerview_id);
+        RecyclerViewAdapter myAdapter = new RecyclerViewAdapter(this,lstBook);
+        myrv.setLayoutManager(new GridLayoutManager(this,3));
+        myrv.setAdapter(myAdapter);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
     }
 }
